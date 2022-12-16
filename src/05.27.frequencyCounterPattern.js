c8a1cade8ad01db85637487d2ab97ccb69600ed6
Kate <kate@gmail.com>
2022-12-16 11:08:15 -0500

    Add frequency solution

--- src/05.27.frequencyCounterPattern.js
+++ src/05.27.frequencyCounterPattern.js
@@ -8,3 +8,43 @@ export const isTheSame = (targetArray, comparedArray) => {
 
   return jointSet.size === targetArray.length;
 };
+
+/**
+ * Strategy: compare frequency and length
+ * @param {*} targetArray
+ * @param {*} comparedArray
+ * @returns boolean
+ */
+export const isTheSameSolution2 = (targetArray, comparedArray) => {
+  if (targetArray.length !== comparedArray.length) {
+    return false;
+  }
+
+  const targetMap = new Map();
+
+  for (let value of targetArray) {
+    const prevFrequency = targetMap.get(value) ?? 0;
+
+    targetMap.set(value, prevFrequency + 1);
+  }
+
+  const comparedMap = new Map();
+
+  for (let value of comparedArray) {
+    const prevFrequency = comparedMap.get(value) ?? 0;
+
+    comparedMap.set(value, prevFrequency + 1);
+  }
+
+  for (let key of targetMap.keys()) {
+    const keyFrequencyInTargetMap = targetMap.get(key);
+    const keyInComparedMap = key * key;
+    const keyFrequencyInComparedMap = comparedMap.get(keyInComparedMap);
+
+    if (keyFrequencyInComparedMap !== keyFrequencyInTargetMap) {
+      return false;
+    }
+  }
+
+  return true;
+};
--- src/05.27.frequencyCounterPattern.test.js
+++ src/05.27.frequencyCounterPattern.test.js
@@ -1,4 +1,4 @@
-import { isTheSame } from "./05.27.frequencyCounterPattern";
+import { isTheSame, isTheSameSolution2 } from "./05.27.frequencyCounterPattern";
 
 describe("isTheSame", () => {
   it("Return false for different size of array", () => {
@@ -11,5 +11,19 @@ describe("isTheSame", () => {
 
   it("Return true matched condition", () => {
     expect(isTheSame([1], [1])).toBe(true);
+  });
+});
+
+describe("isTheSameSolution2", () => {
+  it("return flase for different size of array", () => {
+    expect(isTheSameSolution2([], [2])).toBe(false);
+  });
+
+  it("Return false for the same size yet not match condition", () => {
+    expect(isTheSameSolution2([1], [2])).toBe(false);
+  });
+
+  it("Return true matched condition", () => {
+    expect(isTheSameSolution2([1, 3, 2], [1, 4, 9])).toBe(true);
   });
 });
