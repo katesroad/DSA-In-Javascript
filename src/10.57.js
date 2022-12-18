cc2084430dc2e35b954f648407fd4132559ce9bb
Kate <kate@gmail.com>
2022-12-17 20:28:19 -0500

    add binary search

--- src/10.57.js
+++ src/10.57.js
@@ -1,4 +1,4 @@
-// Big O: O(n)
+// Big O: Worst case: O(n), Best case: O(1), average: O(n)
 export function linearSearch(array, num) {
   for (let index in array) {
     if (array[index] === num) {
@@ -7,4 +7,25 @@ export function linearSearch(array, num) {
   }
 
   return -1;
+}
+
+// sortted array, divide and conquer
+export function binarySearch(array, num) {
+  let leftIndex = 0;
+  let rightIndex = array.length - 1;
+  let middleIndex = Math.floor((rightIndex - leftIndex) / 2 + leftIndex);
+
+  while (array[middleIndex] !== num && leftIndex < rightIndex) {
+    if (array[middleIndex] > num) {
+      rightIndex = middleIndex - 1;
+    } else if (array[middleIndex] < num) {
+      leftIndex = middleIndex + 1;
+    } else {
+      /* empty */
+    }
+
+    middleIndex = Math.floor((rightIndex - leftIndex) / 2 + leftIndex);
+  }
+
+  return array[middleIndex] === num ? middleIndex : -1;
 }
--- src/10.57.test.js
+++ src/10.57.test.js
@@ -1,4 +1,4 @@
-import { linearSearch } from "./10.57";
+import { linearSearch, binarySearch } from "./10.57";
 
 describe("lineaderSearch", () => {
   it("return index if an item is found in array", () => {
@@ -11,6 +11,26 @@ describe("lineaderSearch", () => {
   it("return -1 if no item is found in array", () => {
     const array = [12, -44, 10];
     const num = 1;
+
+    expect(linearSearch(array, num)).toBe(-1);
+  });
+});
+
+describe("binary search", () => {
+  it("return index if an item is found in array", () => {
+    const array1 = [1, 2, 3, 3, 4, 5, 10];
+    const num1 = 10;
+
+    const array2 = [1, 2, 3, 3, 5, 6];
+    const num2 = 6;
+
+    expect(binarySearch(array1, num1)).toBe(6);
+    expect(binarySearch(array2, num2)).toBe(5);
+  });
+
+  it("return -1 if no item is found in array", () => {
+    const array = [-4, 1, 12, 98, 99];
+    const num = 11;
 
     expect(linearSearch(array, num)).toBe(-1);
   });
