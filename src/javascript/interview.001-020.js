58646caab25c6e11b32c2d6eeb40c99985c44632
Kate <kate@gmail.com>
2022-12-20 10:31:37 -0500

    Add js hoc example

--- src/javascript/interview.001-020.js
+++ src/javascript/interview.001-020.js
@@ -16,3 +16,24 @@ sendInvite.apply(firstPerson, ["hello", "how are you"]);
 const invitedPerson = sendInvite.bind(firstPerson);
 
 invitedPerson("hello", "how are you");
+
+// 13 High order function
+
+{
+  // eslint-disable-next-line no-inner-declarations
+  function after(time, callback) {
+    return function () {
+      if (--time == 0) {
+        callback();
+      }
+    };
+  }
+
+  const eat = after(3, () => {
+    console.log("Finished Meal");
+  });
+
+  eat();
+  eat();
+  eat();
+}
--- src/javascript/interview.001-020.test.js
+++ src/javascript/interview.001-020.test.js
@@ -1,3 +1,4 @@
+// 06 && 07
 describe("array methods", () => {
   it("slice", () => {
     const array = [1, 2, 3];
@@ -11,5 +12,24 @@ describe("array methods", () => {
 
     expect(array.splice(0, 1)).toEqual([1]);
     expect(array).toEqual([2, 3]);
+  });
+});
+
+// 08: object vs map
+
+describe("object vs map", () => {
+  it("iterate map", () => {
+    const keys = [];
+    const map = new Map();
+
+    map.set("name", "javascript");
+    map.set("type", "fed");
+
+    // [key, value] of
+    for (let [key] of map) {
+      keys.push(key);
+    }
+
+    expect(keys).toEqual(["name", "type"]);
   });
 });
