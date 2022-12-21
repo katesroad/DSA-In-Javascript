36951916c4fd50f725ae5c998d402a94643165cf
Kate <kate@gmail.com>
2022-12-21 10:27:48 -0500

    encode/decodeURI

---
+++ src/javascript/interview.021-040.test.js
@@ -0,0 +1,11 @@
+describe("quizz", () => {
+  it("encode/decodeURI", () => {
+    let uri = "employeeDetails?name=john&occupation=manager";
+
+    let encoded_uri = encodeURI(uri);
+    let decoded_uri = decodeURI(encoded_uri);
+
+    expect(encoded_uri).toBe("");
+    expect(decoded_uri).toBe("");
+  });
+});
