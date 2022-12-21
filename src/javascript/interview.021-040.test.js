208b8c812f883276a4f2901085691e66b8c74e3a
Kate <kate@gmail.com>
2022-12-21 16:03:21 -0500

    Better format and remove console warning

--- src/javascript/interview.021-040.test.js
+++ src/javascript/interview.021-040.test.js
@@ -1,15 +1,13 @@
 import { parseCookie } from "./interview.021-040";
 
-describe("quizz", () => {
-  it("encode/decodeURI", () => {
-    let uri = "employeeDetails?name=john&occupation=manager";
+it("encode/decodeURI", () => {
+  let uri = "employeeDetails?name=john&occupation=manager";
 
-    let encoded_uri = encodeURI(uri);
-    let decoded_uri = decodeURI(encoded_uri);
+  let encoded_uri = encodeURI(uri);
+  let decoded_uri = decodeURI(encoded_uri);
 
-    expect(encoded_uri).toBe(uri);
-    expect(decoded_uri).toBe(uri);
-  });
+  expect(encoded_uri).toBe(uri);
+  expect(decoded_uri).toBe(uri);
 });
 
 it("parseCookie", () => {
